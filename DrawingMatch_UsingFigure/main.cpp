eb5244da8218a9040bc6b21e867169fdf8a37cf8
ck <[ck@fourthlogic.co.kr]>
2024-02-08 09:46:19 +0900

    add drawing match example about figure / drawing

---
+++ DrawingMatch_UsingFigure/main.cpp
@@ -0,0 +1,250 @@
+#include <cstdio>
+
+#include <FLImaging.h>
+#include "../CommomHeader/ErrorPrint.h"
+
+int main()
+{
+	// 이미지 객체 선언 // Declare the image object
+	CFLImage fliFindImage;
+
+	// 이미지 뷰 선언 // Declare the image view
+	CGUIViewImageWrap viewImageLearn;
+	CGUIViewImageWrap viewImageFind;
+
+	CResult eResult = EResult_UnknownError;
+	CFLFigureArray flfLearntDrawing;
+	CFLFigure* pFlfDrawing = nullptr;
+
+	do
+	{
+		if(IsFail(eResult = fliFindImage.Load(L"../../ExampleImages/Matching/DrawingImage.flif")))
+		{
+			ErrorPrint(eResult, "Failed to load the image file.\n");
+			break;
+		}
+
+		// 이미지 뷰 생성 // Creates imageview
+		if(IsFail(eResult = viewImageLearn.Create(400, 0, 912, 384)))
+		{
+			ErrorPrint(eResult, "Failed to create the image view.\n");
+			break;
+		}
+
+		if(IsFail(eResult = viewImageFind.Create(912, 0, 1680, 576)))
+		{
+			ErrorPrint(eResult, "Failed to create the image view.\n");
+			break;
+		}
+
+		if(IsFail(eResult = viewImageFind.SetImagePtr(&fliFindImage)))
+		{
+			ErrorPrint(eResult, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		CGUIViewImageLayerWrap layerLearn = viewImageLearn.GetLayer(0);
+		CGUIViewImageLayerWrap layerFind = viewImageFind.GetLayer(1);
+
+		layerLearn.Clear();
+		layerFind.Clear();
+
+		if(IsFail(eResult = layerLearn.DrawTextCanvas(&CFLPointD(0, 0), L"LEARN", YELLOW, BLACK, 30)))
+		{
+			ErrorPrint(eResult, "Failed to draw text\n");
+			break;
+		}
+
+		if(IsFail(eResult = layerFind.DrawTextCanvas(&CFLPointD(0, 0), L"FIND", YELLOW, BLACK, 30)))
+		{
+			ErrorPrint(eResult, "Failed to draw text\n");
+			break;
+		}
+
+		// Geometric Match 객체 생성 // Create Geometric Match object
+		CDrawingMatch FLDrawingMatch;
+
+		// 학습할 이미지 설정 // Set the image to learn
+		// 학습할 도면을 설정합니다. // Set the drawing to learn.
+		pFlfDrawing = new CFLFigureArray;
+		pFlfDrawing->Load(L"../../ExampleImages/Matching/Drawing.fig");
+
+		FLDrawingMatch.SetDrawing(pFlfDrawing);
+		// 도면에 대한 분해능 단위를 설정합니다. // Set unit of pixel accuracy.
+		FLDrawingMatch.SetDistanceUnit(CDrawingMatch::EDistanceUnit_Millimeter);
+		// 도면에 대한 분해능을 설정합니다. // Set  pixel accuracy.
+		FLDrawingMatch.SetPixelAccuracy(0.07, 0.07);
+
+		// 학습 파라미터를 설정합니다. // Set the learning parameters.
+		// 추출할 특징점 개수를 설정합니다. // Set the number of feature points to be extracted.
+		FLDrawingMatch.SetFeatureCount();
+		// 추출할 특징점 처리과정에서의 노이즈 필터링 정도를 설정합니다. // Set the noise filtering degree in the process of processing the feature points to be extracted.
+		FLDrawingMatch.SetFeatureFiltering();
+		// 추출할 특징점 처리과정에서의 허용 임계값을 설정합니다. // Set the allowable threshold in the feature point processing process to be extracted.
+		FLDrawingMatch.SetLearnThresholdCoeff();
+		// 추출할 특징점 처리과정에서의 이미지 전처리 유무를 설정합니다. // Set whether or not to pre-process the image in the process of processing the feature points to be extracted.
+		FLDrawingMatch.EnablePreprocessing();
+
+		// 알고리즘 수행 // Execute the algorithm
+		if(IsFail(eResult = FLDrawingMatch.Learn()))
+		{
+			ErrorPrint(eResult, "Failed to execute Learn.\n");
+			break;
+		}
+
+		// 학습한 특징점을 디스플레이 한다 // Display the learned feature point
+		// 학습한 특징점이 찾고자하는 객체를 나타내기에 충분하게 잘 뽑혔는지 확인하고, 그렇지 않다면 학습 파라미터를 재조정함으로써 재확인하면 검출 시 더 효과적입니다. // Check whether the learned feature points are selected well enough to represent the object to be found.
+		flfLearntDrawing = FLDrawingMatch.GetLearntDrawing();
+
+		if(IsFail(eResult = layerLearn.DrawFigureImage(flfLearntDrawing, BLUE)))
+		{
+			ErrorPrint(eResult, "Failed to draw figure\n");
+			break;
+		}
+
+
+		// 검출할 이미지 설정 // Set image to detect
+		FLDrawingMatch.SetSourceImage(fliFindImage);
+
+		// 검출 시 사용될 파라미터를 설정합니다. // Set the parameters to be used for detection.
+		// 검출 시 사용될 기본 각도를 설정합니다. // Set the default angle to be used for detection.
+		FLDrawingMatch.SetAngleBias(0.);
+		// 검출 시 사용될 각도의 탐색범위를 설정합니다. // Set the search range of the angle to be used for detection.
+		// 각도는 기본 각도를 기준으로 (기본 각도 - AngleTolerance, 기본 각도 + AngleTolerance)가 최종 탐색범위 // The angle is based on the basic angle (default angle - AngleTolerance, basic angle + AngleTolerance) is the final search range
+		FLDrawingMatch.SetAngleTolerance(5.);
+		// 검출 시 사용될 스케일 탐색범위를 설정합니다. // Set the scale search range to be used for detection.
+		FLDrawingMatch.SetScaleRange(0.5, 1.2);
+		// 검출 시 사용될 최소 탐색점수를 설정합니다. // Set the minimum search score to be used for detection.
+		FLDrawingMatch.SetMinimumDetectionScore(0.5);
+		// 검출 시 사용될 최대 탐색객체 수를 설정합니다. // Set the maximum number of search objects to be used for detection.
+		FLDrawingMatch.SetMaxObject(10);
+
+		// 검출 시 보간법 사용 유무에 대해 설정합니다. // Set whether to use interpolation when detecting.
+		FLDrawingMatch.EnableInterpolation();
+		// 검출 시 최적화 정도에 대해 설정합니다. // Set the degree of optimization for detection.
+		FLDrawingMatch.SetOptimizationOption();
+		// 검출 시 대비정도에 대해 설정합니다. // Set the contrast level for detection.
+		FLDrawingMatch.SetConstrastOption();
+		// 검출 시 이미지 영역밖의 탐색 정도를 설정합니다. // Set the degree of search outside the image area when detecting.
+		FLDrawingMatch.SetInvisibleRegionEstimation();
+		// 검출 시 처리과정에서의 허용 임계값을 설정합니다. // Set the allowable threshold in the process of detection.
+		FLDrawingMatch.SetFindThresholdCoeff();
+		// 검출 시 겹쳐짐 허용 정도를 설정합니다. // Set the allowable degree of overlap during detection.
+		FLDrawingMatch.SetObjectOverlap();
+		// 검출 시 이미지 전처리 유무를 설정합니다. // Set whether or not to pre-process the image during detection.
+		// 학습과정에서 전처리 유무를 설정과 동일하게 설정하는 것을 추천합니다. // In the learning process, it is recommended to set the pre-processing status the same as the setting.
+		FLDrawingMatch.EnablePreprocessing();
+
+		// 알고리즘 수행 // Execute the algorithm
+		eResult = FLDrawingMatch.Execute();
+
+		if(IsFail(eResult))
+		{
+			ErrorPrint(eResult, "Failed to Execute\n");
+			break;
+		}
+
+
+		// 기하학적 패턴 검출 결과를 가져옵니다. // Get the geometric pattern detection result.
+		std::vector<CDrawingMatch::SResult> vctResults;
+		int64_t i64ResultCount = FLDrawingMatch.GetResultCount();
+
+		printf(" ▶ Find Information\n");
+
+		for(int64_t i = 0; i < i64ResultCount; ++i)
+		{
+			CDrawingMatch::SResult results;
+			FLDrawingMatch.GetResult(i, results);
+
+			bool bInverse = results.bInverse;
+			float f32Score = results.f32Score;
+			float f32Angle = results.f32Angle;
+			float f32Scale = results.f32Scale;
+			Foundation::CFLFigure* pFlfRegion = results.pFlfRegion;
+			Foundation::CFLPointD flpLocation = results.flpLocation;
+			Foundation::CFLPointD flpPivot = results.flpPivot;
+
+			CFLString<wchar_t> strResult;
+			CFLString<wchar_t> strInverse = bInverse ? L"Inverse Type" : L"Normal Type";
+
+			CFLRectD flrResultRegion = pFlfRegion->GetBoundaryRect();
+
+			// 기하학적 패턴 검출 결과를 Console창에 출력합니다. // Output the geometric pattern detection result to the console window.
+			printf(" < Instance : %lld >\n", i);
+			printf("  1. ROI Shape Type : Rectangle\n");
+			printf("    left   : %.3lf\n", flrResultRegion.left);
+			printf("    right  : %.3lf\n", flrResultRegion.right);
+			printf("    top    : %.3lf\n", flrResultRegion.top);
+			printf("    bottom : %.3lf\n", flrResultRegion.bottom);
+			printf("    angle  : %.3lf\n", f32Angle);
+			printf("  2. Interest Pivot : (%.3lf, %.3lf)\n", flpPivot.x, flpPivot.y);
+			printf("  3. Score : %0.3lf\n  4. Angle : %0.3lf\n  5. Scale : %0.3lf\n", f32Score, f32Angle, f32Scale);
+
+			if(bInverse)
+				printf("  6. Contrast : Inverse Type\n");
+			else
+				printf("  6. Contrast : Normal Type\n");
+
+			printf("\n");
+
+			// 검출 결과의 중심점을 디스플레이 한다 // Display the center point of the detection result
+			CFLFigureArray flfaPoint = flpPivot.MakeCrossHair(3, false);
+			flfaPoint.Rotate(f32Angle, &flpPivot);
+
+			if(IsFail(eResult = layerFind.DrawFigureImage(&flfaPoint, BLACK, 3)))
+			{
+				ErrorPrint(eResult, "Failed to draw figure\n");
+				break;
+			}
+
+			if(IsFail(eResult = layerFind.DrawFigureImage(&flfaPoint, LIME)))
+			{
+				ErrorPrint(eResult, "Failed to draw figure\n");
+				break;
+			}
+
+			if(IsFail(eResult = layerFind.DrawFigureImage(pFlfRegion, BLACK, 3)))
+			{
+				ErrorPrint(eResult, "Failed to draw figure\n");
+				break;
+			}
+
+			if(IsFail(eResult = layerFind.DrawFigureImage(pFlfRegion, CYAN)))
+			{
+				ErrorPrint(eResult, "Failed to draw figure\n");
+				break;
+			}
+
+			TPoint<double> tpPosition;
+			tpPosition.x = flpPivot.x;
+			tpPosition.y = flpPivot.y;
+
+			CFLString<wchar_t> strText;
+			strText.Format(L"Score : %.3lf\nAngle : %.3lf\nScale : x%.3lf\n", f32Score, f32Angle, f32Scale);
+
+			if((eResult = layerFind.DrawTextImage(tpPosition, strText, YELLOW, BLACK, 15, false, 0, EGUIViewImageTextAlignment_LEFT_CENTER)).IsFail())
+			{
+				ErrorPrint(eResult, "Failed to draw text");
+				break;
+			}
+		}
+
+		// 이미지 뷰를 갱신 합니다. // Update the image view.
+		viewImageLearn.ZoomFitToLayer(0);
+		viewImageLearn.Invalidate(true);
+		viewImageFind.Invalidate(true);
+
+		// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+		while(viewImageLearn.IsAvailable() && viewImageFind.IsAvailable())
+			CThreadUtilities::Sleep(1);
+	}
+	while(false);
+
+	if(pFlfDrawing)
+	{
+		delete pFlfDrawing;
+		pFlfDrawing = nullptr;
+	}
+
+	return 0;
+}
