712e218d9e1db6320f4fe88b34c0bd4c31bffce4
jmyu <jmyu@fourthlogic.co.kr>
2024-04-08 14:26:03 +0900

    Add ColorBoosting Example

--- ColorBoosting/main.cpp
+++ ColorBoosting/main.cpp
@@ -17,90 +17,90 @@ int main()
 	do
 	{
 		// 동작 결과 // operation result
-		CResult res = EResult_UnknownError;
+		CResult eResult = EResult_UnknownError;
 
 		// 이미지 로드 // Loads image
-		if(IsFail(res = fliSourceImage.Load(L"../../ExampleImages/ColorBoosting/Sand.flif")))
+		if(IsFail(eResult = fliSourceImage.Load(L"../../ExampleImages/Decolorization/Pattern.flif")))
 		{
-			ErrorPrint(res, "Failed to load the image file.\n");
+			ErrorPrint(eResult, "Failed to load the image file.\n");
 			break;
 		}
 
 		// 이미지 뷰 생성 // Create image view
-		if(IsFail(res = viewImageSource.Create(112, 0, 912, 534)))
+		if(IsFail(eResult = viewImageSource.Create(112, 0, 912, 534)))
 		{
-			ErrorPrint(res, "Failed to create the image view.\n");
+			ErrorPrint(eResult, "Failed to create the image view.\n");
 			break;
 		}
 
-		if(IsFail(res = viewImageConverted.Create(913, 0, 1713, 534)))
+		if(IsFail(eResult = viewImageConverted.Create(913, 0, 1713, 534)))
 		{
-			ErrorPrint(res, "Failed to create the image view.\n");
+			ErrorPrint(eResult, "Failed to create the image view.\n");
 			break;
 		}
 
 		// 이미지 뷰에 이미지를 디스플레이 // Display the image in the image view
-		if(IsFail(res = viewImageSource.SetImagePtr(&fliSourceImage)))
+		if(IsFail(eResult = viewImageSource.SetImagePtr(&fliSourceImage)))
 		{
-			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			ErrorPrint(eResult, "Failed to set image object on the image view.\n");
 			break;
 		}
 
-		if(IsFail(res = viewImageConverted.SetImagePtr(&fliConvertedImage)))
+		if(IsFail(eResult = viewImageConverted.SetImagePtr(&fliConvertedImage)))
 		{
-			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			ErrorPrint(eResult, "Failed to set image object on the image view.\n");
 			break;
 		}
 
 		// 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views. 
-		if(IsFail(res = viewImageSource.SynchronizePointOfView(&viewImageConverted)))
+		if(IsFail(eResult = viewImageSource.SynchronizePointOfView(&viewImageConverted)))
 		{
-			ErrorPrint(res, "Failed to synchronize view\n");
+			ErrorPrint(eResult, "Failed to synchronize view\n");
 			break;
 		}
 
 		// 두 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the two image view windows
-		if(IsFail(res = viewImageSource.SynchronizeWindow(&viewImageConverted)))
+		if(IsFail(eResult = viewImageSource.SynchronizeWindow(&viewImageConverted)))
 		{
-			ErrorPrint(res, "Failed to synchronize window\n");
+			ErrorPrint(eResult, "Failed to synchronize window\n");
 			break;
 		}
 
-		// ColorBoosting 객체 생성 // Create ColorBoosting object
-		CColorBoosting ColorBoosting;
+		// Decolorization 객체 생성 // Create Decolorization object
+		CDecolorization Decolorization;
 
 		// Source 이미지 설정 // Set source image 
-		if(IsFail(res = ColorBoosting.SetSourceImage(fliSourceImage)))
+		if(IsFail(eResult = Decolorization.SetSourceImage(fliSourceImage)))
 		{
-			ErrorPrint(res, "Failed to set Source Image.");
+			ErrorPrint(eResult, "Failed to set Source Image.");
 			break;
 		}
 
 		// Destination 이미지 설정 // Set destination image
-		if(IsFail(res = ColorBoosting.SetDestinationImage(fliConvertedImage)))
+		if(IsFail(eResult = Decolorization.SetDestinationImage(fliConvertedImage)))
 		{
-			ErrorPrint(res, "Failed to set Destination Image.");
+			ErrorPrint(eResult, "Failed to set Destination Image.");
 			break;
 		}
 
 		// 연산 속도 설정 // Set the operation speed.
-		if(IsFail(res = ColorBoosting.SetOperationSpeed(CColorBoosting::EOperationSpeed_Fast)))
+		if(IsFail(eResult = Decolorization.SetOperationSpeed(EOperationSpeed_Normal)))
 		{
-			ErrorPrint(res, "Failed to set Operation Speed.");
+			ErrorPrint(eResult, "Failed to set Operation Speed.");
 			break;
 		}
 
 		// 알고리즘 수행 // Execute the algorithm
-		if((res = ColorBoosting.Execute()).IsFail())
+		if((eResult = Decolorization.Execute()).IsFail())
 		{
-			ErrorPrint(res, "Failed to process.");
+			ErrorPrint(eResult, "Failed to process.");
 			break;
 		}
 
 		// 이미지 뷰의 zoom fit // image view zoom fit
-		if((res = viewImageConverted.ZoomFit()).IsFail())
+		if((eResult = viewImageConverted.ZoomFit()).IsFail())
 		{
-			ErrorPrint(res, "Failed to zoom fit\n");
+			ErrorPrint(eResult, "Failed to zoom fit\n");
 			break;
 		}
 
@@ -114,15 +114,15 @@ int main()
 		layerConverted.Clear();
 
 		// View 정보를 디스플레이 합니다. // Display View information.
-		if(IsFail(res = layerSource.DrawTextCanvas(&CFLPointD(0, 0), L"Source Image", YELLOW, BLACK, 30)))
+		if(IsFail(eResult = layerSource.DrawTextCanvas(&CFLPointD(0, 0), L"Source Image", YELLOW, BLACK, 30)))
 		{
-			ErrorPrint(res, "Failed to draw text\n");
+			ErrorPrint(eResult, "Failed to draw text\n");
 			break;
 		}
 
-		if(IsFail(res = layerConverted.DrawTextCanvas(&CFLPointD(0, 0), L"Converted Image", YELLOW, BLACK, 30)))
+		if(IsFail(eResult = layerConverted.DrawTextCanvas(&CFLPointD(0, 0), L"Converted Image", YELLOW, BLACK, 30)))
 		{
-			ErrorPrint(res, "Failed to draw text\n");
+			ErrorPrint(eResult, "Failed to draw text\n");
 			break;
 		}
 
