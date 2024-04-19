701e4ccd354438ecd8331a9ac7b1e045beae4129
ck <[ck@fourthlogic.co.kr]>
2024-04-19 20:26:54 +0900

    Add perspective transform 3d example

---
+++ PerspectiveTransform3D/main.cpp
@@ -0,0 +1,108 @@
+#include <cstdio>
+#include <FLImaging.h>
+#include "../CommomHeader/ErrorPrint.h"
+
+int main()
+{
+	// 이미지 뷰 선언 // Declare image view
+	CGUIView3DWrap view3DSrc;
+	CGUIView3DWrap view3DDst;
+	CFL3DObject fl3DObjectSrc;
+	CFL3DObject fl3DObjectDst;
+
+	do
+	{
+		// 알고리즘 동작 결과 // Algorithm execution result
+		CResult res = EResult_UnknownError;
+
+		// Source 3D 이미지 뷰 생성 // Create the Source 3D image view
+		if((res = view3DSrc.Create(400, 200, 1300, 800)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to create the image view.\n");
+			break;
+		}
+
+		// Destination 3D 이미지 뷰 생성 // Create the destination 3D image view
+		if((res = view3DDst.Create(400, 200, 1300, 800)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to create the image view.\n");
+			break;
+		}
+
+		CPlyReader plyReader;
+		plyReader.Load(L"../../ExampleImages/PerspectiveTransform3D/binary-vertex.ply");
+		plyReader.GetResult3DObject(fl3DObjectSrc);
+
+		// Distance Transform 3D 객체 생성 // Create Distance Transform 3D object
+		CPerspectiveTransform3D PerspectiveTransform3D;
+
+		TPoint3<float> tpPosition = TPoint3<float>(0.000000, 0.000000, 0.000000);
+		TPoint3<float> tpDirection = TPoint3<float>(-0.100000, 0.000000, -1.000000);
+		TPoint3<float> tpUpVector = TPoint3<float>(0.000000, 1.000000, 0.000000);
+
+		// Source 객체 설정 // Set the source object
+		PerspectiveTransform3D.SetSourceObject(&fl3DObjectSrc);
+		// Destionation 객체 설정 // Set the Destionation object
+		PerspectiveTransform3D.SetDestinationObject(&fl3DObjectDst);
+		// 카메라 위치 설정 // Set the camera position
+		PerspectiveTransform3D.SetPosition(tpPosition);
+		// 카메라 방향 설정 // Set the camera direction
+		PerspectiveTransform3D.SetDirection(tpDirection);
+		// 카메라 방향 타입 설정 // Set the camera direction type
+		PerspectiveTransform3D.SetDirectionType(CPerspectiveTransform3D::EDirectionType_Decrement);
+		// 카메라 업 벡터 설정 // Set the camera up vector
+		PerspectiveTransform3D.SetUpVector(tpUpVector);
+
+		// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+		if((res = PerspectiveTransform3D.Execute()).IsFail())
+		{
+			ErrorPrint(res, L"Failed to execute multiFocus.");
+			break;
+		}
+
+		// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+		// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+		CGUIView3DLayerWrap layer3DSrc = view3DSrc.GetLayer(0);
+		CGUIView3DLayerWrap layer3DDst = view3DDst.GetLayer(0);
+
+		// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+		layer3DSrc.Clear();
+		layer3DDst.Clear();
+
+		// Destination 이미지가 새로 생성됨으로 Zoom fit 을 통해 디스플레이 되는 이미지 배율을 화면에 맞춰준다. // With the newly created Destination image, the image magnification displayed through Zoom fit is adjusted to the screen.
+		if((res = view3DDst.ZoomFit()).IsFail())
+		{
+			ErrorPrint(res, L"Failed to zoom fit of the image view.\n");
+			break;
+		}
+
+		view3DSrc.PushObject(fl3DObjectSrc);
+		view3DSrc.ZoomFit();
+
+		view3DDst.PushObject(fl3DObjectDst);
+		view3DDst.ZoomFit();
+
+		if((res = layer3DSrc.DrawTextCanvas(&CFLPointD(0, 0), L"Source Object", YELLOW, BLACK, 20)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to draw text.\n");
+			break;
+		}
+
+		if((res = layer3DDst.DrawTextCanvas(&CFLPointD(0, 0), L"Destination Object", YELLOW, BLACK, 20)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to draw text.\n");
+			break;
+		}
+
+		// 이미지 뷰를 갱신 합니다. // Update image view
+		view3DSrc.Invalidate(true);
+		view3DDst.Invalidate(true);
+
+		// 이미지 뷰, 3D 뷰가 종료될 때 까지 기다림
+		while(view3DSrc.IsAvailable() || view3DDst.IsAvailable())
+			CThreadUtilities::Sleep(1);
+	}
+	while(false);
+
+	return 0;
+}
