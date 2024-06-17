82060117142aa25b682b9ada2305267cfb9cc9db
jslee <jslee@fourthlogic.co.kr>
2024-06-17 20:53:48 +0900

    Add Bilateral Filter Grid Example

---
+++ BilateralFilterGrid/main.cpp
@@ -0,0 +1,134 @@
+#include <cstdio>
+
+#include <FLImaging.h>
+#include "../CommomHeader/ErrorPrint.h"
+
+int main()
+{
+	// 이미지 객체 선언 // Declare image object
+	CFLImage fliSourceImage;
+	CFLImage fliDestinationImage;
+
+	// 이미지 뷰 선언 // Declare image view
+	CGUIViewImageWrap viewImageSource;
+	CGUIViewImageWrap viewImageDestination;
+
+	// 수행 결과 객체 선언 // Declare the execution result object
+	CResult res = EResult_UnknownError;
+
+	do
+	{
+		// Source 이미지 로드 // Load the source image
+		if((res = fliSourceImage.Load(L"../../ExampleImages/BilateralFilterGrid/Gear.flif")).IsFail())
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		// Destination 이미지 로드 // Load the destination image
+		if((res = fliDestinationImage.Load(L"../../ExampleImages/BilateralFilterGrid/Gear.flif")).IsFail())
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		// Source 이미지 뷰 생성 // Create Source image view
+		if((res = viewImageSource.Create(100, 0, 550, 480)).IsFail())
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// Destination 이미지 뷰 생성 // Create destination image view
+		if((res = viewImageDestination.Create(1000, 0, 1450, 480)).IsFail())
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// Source 이미지 뷰와 Destination 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the source view and the destination view
+		if((res = viewImageSource.SynchronizePointOfView(&viewImageDestination)).IsFail())
+		{
+			ErrorPrint(res, "Failed to synchronize view\n");
+			break;
+		}
+
+		// Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
+		if((res = viewImageSource.SetImagePtr(&fliSourceImage)).IsFail())
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// Destination 이미지 뷰에 이미지를 디스플레이 // Display the image in the destination image view
+		if((res = viewImageDestination.SetImagePtr(&fliDestinationImage)).IsFail())
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// 두 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the two image view windows
+		if((res = viewImageSource.SynchronizeWindow(&viewImageDestination)).IsFail())
+		{
+			ErrorPrint(res, "Failed to synchronize window.\n");
+			break;
+		}
+
+		// BilateralFilterGrid 객체 생성 // Create CBilateralFilterGrid object
+		CBilateralFilterGrid bilateralFilterGrid;
+
+		// Source 이미지 설정 // Set the source image
+		bilateralFilterGrid.SetSourceImage(fliSourceImage);
+
+		// Destination 이미지 설정 // Set the destination image
+		bilateralFilterGrid.SetDestinationImage(fliDestinationImage);
+
+		// Sampling Rate Spatial 설정 // Set the Sampling Rate Spatial
+		bilateralFilterGrid.SetSamplingRateSpatial(8);
+
+		// Sampling Rate Range 설정 // Set the Sampling Rate Range
+		bilateralFilterGrid.SetSamplingRateRange(0.1);
+
+		// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+		if((res = bilateralFilterGrid.Execute()).IsFail())
+		{
+			ErrorPrint(res, "Failed to execute bilateral filter.\n");
+			wprintf(res.GetString());
+			ErrorPrint(res, "\n");
+			break;
+		}
+
+		// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+		// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+		CGUIViewImageLayerWrap layerSource = viewImageSource.GetLayer(0);
+		CGUIViewImageLayerWrap layerDestination = viewImageDestination.GetLayer(0);
+
+		// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+		layerSource.Clear();
+		layerDestination.Clear();
+
+		// 이미지 뷰 정보 표시 // Display image view information
+		if((res = layerSource.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Source Image", YELLOW, BLACK, 30)).IsFail())
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		if((res = layerDestination.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Destination Image", YELLOW, BLACK, 30)).IsFail())
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		// 이미지 뷰를 갱신 // Update image view
+		viewImageSource.Invalidate(true);
+		viewImageDestination.Invalidate(true);
+
+		// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+		while(viewImageSource.IsAvailable() && viewImageDestination.IsAvailable())
+			CThreadUtilities::Sleep(1);
+	}
+	while(false);
+
+	return 0;
+}
