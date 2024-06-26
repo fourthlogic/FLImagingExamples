2292b76ed86d5fffa600594537469467ce58ad19
jhkim2 <jhkim2@fourthlogic.co.kr>
2024-06-26 19:47:36 +0900

    Add Operation Trailing Ones Example

---
+++ OperationTrailingOnes/main.cpp
@@ -0,0 +1,129 @@
+#include <cstdio>
+
+#include <FLImaging.h>
+#include "../CommomHeader/ErrorPrint.h"
+
+
+int main()
+{
+	// 이미지 객체 선언 // Declare image object
+	CFLImage fliSourceImage;
+	CFLImage fliDestinationImage;
+
+	// 이미지 뷰 선언 // Declare image view
+	CGUIViewImageWrap viewImageSrc;
+	CGUIViewImageWrap viewIamgeDst;
+
+	 // 수행 결과 객체 선언 // Declare the execution result object
+	CResult res = EResult_UnknownError;
+
+	do
+	{
+		// Source 이미지 로드 // Load the source image
+		if((res = fliSourceImage.Load(L"../../ExampleImages/OperationTrailingOnes/Src.flif")).IsFail())
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		// Destination 이미지를 Source 이미지와 동일하게 설정 // Assign the Source image to Destination image
+		if((res = fliDestinationImage.Assign(fliSourceImage)).IsFail())
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		// Source 이미지 뷰 생성 // Create Source image view
+		if((res = viewImageSrc.Create(100, 0, 600, 545)).IsFail())
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// Destination 이미지 뷰 생성 // Create the Destination image view
+		if((res = viewIamgeDst.Create(600, 0,1100, 545)).IsFail())
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// 두 이미지 뷰의 시점을 동기화한다 // Synchronize the viewpoints of the two image views
+		if((res = viewImageSrc.SynchronizePointOfView(&viewIamgeDst)).IsFail())
+		{
+			ErrorPrint(res, "Failed to synchronize view\n");
+			break;
+		}
+
+		// Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
+		if((res = viewImageSrc.SetImagePtr(&fliSourceImage)).IsFail())
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// Destination 이미지 뷰에 이미지를 디스플레이 // Display the image in the destination image view
+		if((res = viewIamgeDst.SetImagePtr(&fliDestinationImage)).IsFail())
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// 두 이미지 뷰 윈도우의 위치를 동기화한다 // Synchronize the positions of the two image view windows
+		if((res = viewImageSrc.SynchronizeWindow(&viewIamgeDst)).IsFail())
+		{
+			ErrorPrint(res, "Failed to synchronize window.\n");
+			break;
+		}
+
+		// Operation Trailing Ones 객체 생성 // Create Operation Trailing Ones object
+		COperationTrailingOnes to;
+
+		// Source 이미지 설정 // Set the source image
+		to.SetSourceImage(fliSourceImage);
+
+		// Destination 이미지 설정 // Set the destination image
+		to.SetDestinationImage(fliDestinationImage);
+
+		// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+		if((res = to.Execute()).IsFail())
+		{
+			ErrorPrint(res, "Failed to execute operation trailing ones.");
+			break;
+		}
+
+		// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+		// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+		CGUIViewImageLayerWrap layerSource = viewImageSrc.GetLayer(0);
+		CGUIViewImageLayerWrap layerDestination = viewIamgeDst.GetLayer(0);
+
+		// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+		layerSource.Clear();
+		layerDestination.Clear();
+
+		// 이미지 뷰 정보 표시 // Display image view information
+		CFLPoint<double> flpPoint = CFLPoint<double>(0, 0);
+
+		if((res = layerSource.DrawTextCanvas(&flpPoint, L"Source Image", YELLOW, BLACK, 20)).IsFail())
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		if((res = layerDestination.DrawTextCanvas(&flpPoint, L"Destination Image(Multiplied by 2 + 1i)", YELLOW, BLACK, 20)).IsFail())
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		// 이미지 뷰를 갱신 // Update image view
+		viewImageSrc.Invalidate(true);
+		viewIamgeDst.Invalidate(true);
+
+		// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+		while(viewImageSrc.IsAvailable() && viewIamgeDst.IsAvailable())
+			CThreadUtilities::Sleep(1);
+	}
+	while(false);
+
+	return 0;
+}
