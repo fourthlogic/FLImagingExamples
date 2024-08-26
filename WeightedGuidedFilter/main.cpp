07f6bcbddc3dbc051bdc2f11b6749726486cfe5d
swbang <swbang@fourthlogic.co.kr>
2024-08-26 12:20:30 +0900

    Add Weighted Guided Filter Example

---
+++ WeightedGuidedFilter/main.cpp
@@ -0,0 +1,186 @@
+#include <cstdio>
+
+#include <FLImaging.h>
+#include "../CommomHeader/ErrorPrint.h"
+
+
+int main()
+{
+	// 이미지 객체 선언 // Declare image object
+	CFLImage fliSourceImage;
+	CFLImage fliOperandImage;
+	CFLImage fliDestinationImage;
+
+	// 이미지 뷰 선언 // Declare image view
+	CGUIViewImageWrap viewImageSource;
+	CGUIViewImageWrap viewImageOperand;
+	CGUIViewImageWrap viewImageDestination;
+
+	CResult res = EResult_UnknownError;
+
+	do
+	{
+		// Source 이미지 로드 // Load the source image
+		if((res = fliSourceImage.Load(L"../../ExampleImages/WeightedGuidedFilter/cat_450x480.flif")).IsFail())
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		// Operand 이미지 로드 // Loads the operand image
+		if((res = fliOperandImage.Load(L"../../ExampleImages/WeightedGuidedFilter/cat_450x480.flif")).IsFail())
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		// Destination 이미지 로드 // Load the destination image
+		if((res = fliDestinationImage.Load(L"../../ExampleImages/WeightedGuidedFilter/cat_450x480.flif")).IsFail())
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		// Source 이미지 뷰 생성 // Create Source image view
+		if((res = viewImageSource.Create(100, 0, 550, 480)).IsFail())
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// Operand 이미지 뷰 생성 // Creates operand image view
+		if((res = viewImageOperand.Create(550, 0, 1000, 480)).IsFail())
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// Destination 이미지 뷰 생성 // Create destination image view
+		if((res = viewImageDestination.Create(1000, 0, 1450, 480)).IsFail())
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// Source 이미지 뷰와 Operand 이미지 뷰의 시점을 동기화 한다
+		if((res = viewImageSource.SynchronizePointOfView(&viewImageOperand)).IsFail())
+		{
+			ErrorPrint(res, "Failed to synchronize view\n");
+			break;
+		}
+
+		// Source 이미지 뷰와 Destination 이미지 뷰의 시점을 동기화 한다
+		if((res = viewImageSource.SynchronizePointOfView(&viewImageDestination)).IsFail())
+		{
+			ErrorPrint(res, "Failed to synchronize view\n");
+			break;
+		}
+
+		// Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
+		if((res = viewImageSource.SetImagePtr(&fliSourceImage)).IsFail())
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// Operand 이미지 뷰에 이미지를 디스플레이
+		if((res = viewImageOperand.SetImagePtr(&fliOperandImage)).IsFail())
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// Destination 이미지 뷰에 이미지를 디스플레이 // Display the image in the destination image view
+		if((res = viewImageDestination.SetImagePtr(&fliDestinationImage)).IsFail())
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// 두 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the two image view windows
+		if((res = viewImageSource.SynchronizeWindow(&viewImageOperand)).IsFail())
+		{
+			ErrorPrint(res, "Failed to synchronize window.\n");
+			break;
+		}
+
+		// 두 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the two image view windows
+		if((res = viewImageSource.SynchronizeWindow(&viewImageDestination)).IsFail())
+		{
+			ErrorPrint(res, "Failed to synchronize window.\n");
+			break;
+		}
+
+		//알고리즘 동작 결과
+		CResult res = EResult_UnknownError;
+
+		// WeightedGuidedFilter 객체 생성 // Create WeightedGuidedFilter object
+		CWeightedGuidedFilter weightedGuidedFilter;
+
+		// Source 이미지 설정 // Set the source image
+		weightedGuidedFilter.SetSourceImage(fliSourceImage);
+
+		// Operand 이미지 설정 // Set the operand image
+		weightedGuidedFilter.SetOperandImage(fliOperandImage);
+
+		// Destination 이미지 설정 // Set the destination image
+		weightedGuidedFilter.SetDestinationImage(fliDestinationImage);
+
+		// filter mask radius = 3 설정
+		weightedGuidedFilter.SetRadius(3);
+
+		// amplitude = 30 설정
+		weightedGuidedFilter.SetAmplitude(30);
+
+		// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+		if((res = weightedGuidedFilter.Execute()).IsFail())
+		{
+			ErrorPrint(res, "Failed to execute weighted guided filter.\n");
+			wprintf(res.GetString());
+			ErrorPrint(res, "\n");
+			break;
+		}
+
+		// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+		// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+		CGUIViewImageLayerWrap layerSource = viewImageSource.GetLayer(0);
+		CGUIViewImageLayerWrap layerOperand = viewImageOperand.GetLayer(0);
+		CGUIViewImageLayerWrap layerDestination = viewImageDestination.GetLayer(0);
+
+		// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+		layerSource.Clear();
+		layerOperand.Clear();
+		layerDestination.Clear();
+
+		// 이미지 뷰 정보 표시 // Display image view information
+		if((res = layerSource.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Source Image", YELLOW, BLACK, 30)).IsFail())
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		if((res = layerOperand.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Operand Image", YELLOW, BLACK, 30)).IsFail())
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		if((res = layerDestination.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Destination Image", YELLOW, BLACK, 30)).IsFail())
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		// 이미지 뷰를 갱신 // Update image view
+		viewImageSource.Invalidate(true);
+		viewImageOperand.Invalidate(true);
+		viewImageDestination.Invalidate(true);
+
+		// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+		while(viewImageSource.IsAvailable() && viewImageOperand.IsAvailable() && viewImageDestination.IsAvailable())
+			CThreadUtilities::Sleep(1);
+	}
+	while(false);
+
+	return 0;
+}
