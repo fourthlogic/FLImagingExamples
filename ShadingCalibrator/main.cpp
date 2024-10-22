4d5eeb54b3cd3ad8cd54fa9658eb6eeec78da107
swbang <swbang@fourthlogic.co.kr>
2024-10-22 17:12:41 +0900

    Add ShadingCalibrator Example

---
+++ ShadingCalibrator/main.cpp
@@ -0,0 +1,190 @@
+#include <cstdio>
+
+#include <FLImaging.h>
+#include "../CommomHeader/ErrorPrint.h"
+
+
+int main()
+{
+	// 이미지 객체 선언 // Declare image object
+	CFLImage fliLearnImage;
+	CFLImage fliSrcImage;
+	CFLImage fliDstImage;
+
+	// 이미지 뷰 선언 // Declare image view
+	CGUIViewImageWrap viewImageLearn;
+	CGUIViewImageWrap viewImageSrc;
+	CGUIViewImageWrap viewImageDst;
+
+	// 알고리즘 동작 결과 // Algorithm execution result
+	CResult res = EResult_UnknownError;
+
+	do
+	{
+		// Learn 이미지 로드 // Load the learn image
+		if((res = fliLearnImage.Load(L"../../ExampleImages/ShadingCalibrator/Learn.flif")).IsFail())
+		{
+			ErrorPrint(res, L"Failed to load the image file.\n");
+			break;
+		}
+
+		// Learn 이미지 뷰 생성 // Create the learn image view
+		if((res = viewImageLearn.Create(400, 0, 912, 612)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to create the image view.\n");
+			break;
+		}
+
+		// Learn 이미지 뷰에 이미지를 디스플레이 // Display the image in the learn image view
+		if((res = viewImageLearn.SetImagePtr(&fliLearnImage)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// Source 이미지 로드 // Load the source image
+		if((res = fliSrcImage.Load(L"../../ExampleImages/ShadingCalibrator/Src.flif")).IsFail())
+		{
+			ErrorPrint(res, L"Failed to load the image file.\n");
+			break;
+		}
+
+		// Source 이미지 뷰 생성 // Create the source image view
+		if((res = viewImageSrc.Create(912, 0, 1424, 612)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to create the image view.\n");
+			break;
+		}
+
+		// Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
+		if((res = viewImageSrc.SetImagePtr(&fliSrcImage)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// Destination 이미지 뷰 생성 // Create the destination image view
+		if((res = viewImageDst.Create(1424, 0, 1936, 612)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to create the image view.\n");
+			break;
+		}
+
+		// Destination 이미지 뷰에 이미지를 디스플레이 // Display the image in the destination image view
+		if((res = viewImageDst.SetImagePtr(&fliDstImage)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views
+		if((res = viewImageSrc.SynchronizePointOfView(&viewImageDst)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to synchronize view\n");
+			break;
+		}
+
+		// 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views
+		if((res = viewImageSrc.SynchronizePointOfView(&viewImageLearn)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to synchronize view\n");
+			break;
+		}
+
+		// 두 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the two image view windows
+		if((res = viewImageSrc.SynchronizeWindow(&viewImageDst)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to synchronize window.\n");
+			break;
+		}
+
+		// 두 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the two image view windows
+		if((res = viewImageSrc.SynchronizeWindow(&viewImageLearn)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to synchronize window.\n");
+			break;
+		}
+
+		// ShadingCalibrator 객체 생성 // Create ShadingCalibrator object
+		CShadingCalibrator ShadingCalibrator;
+
+		// Learn 이미지 설정 // Set the learn image
+		ShadingCalibrator.SetLearnImageForShadingCalibration(fliLearnImage);
+
+		// 앞서 설정된 이미지로 Calibration을 수행 // Perform Calibration with the previously set image
+		if((res = ShadingCalibrator.Calibrate()))
+		{
+			ErrorPrint(res, L"Failed to calibrate.\n");
+			break;
+		}
+
+		// Source 이미지 설정 // Set the source image
+		ShadingCalibrator.SetSourceImage(fliSrcImage);
+		// Destination 이미지 설정 // Set the destination image
+		ShadingCalibrator.SetDestinationImage(fliDstImage);
+		// Batch Processing 설정 // Set Batch Processing
+		ShadingCalibrator.EnableBatchProcessing(true);
+
+		// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+		if((res = ShadingCalibrator.Execute()).IsFail())
+		{
+			ErrorPrint(res, L"Failed to execute ShadingCalibrator.");
+			break;
+		}
+
+		// Destination 이미지가 새로 생성됨으로 Zoom fit 을 통해 디스플레이 되는 이미지 배율을 화면에 맞춰준다. // With the newly created Destination image, the image magnification displayed through Zoom fit is adjusted to the screen.
+		if((res = viewImageDst.ZoomFit()).IsFail())
+		{
+			ErrorPrint(res, L"Failed to zoom fit of the image view.\n");
+			break;
+		}
+
+		// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+		// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+		CGUIViewImageLayerWrap layerLearn = viewImageLearn.GetLayer(0);
+		CGUIViewImageLayerWrap layerSrc = viewImageSrc.GetLayer(0);
+		CGUIViewImageLayerWrap layerDst = viewImageDst.GetLayer(0);
+
+		// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+		layerLearn.Clear();
+		layerSrc.Clear();
+		layerDst.Clear();
+
+		// View 정보를 디스플레이 한다. // Display view information
+		// 아래 함수 DrawTextCanvas 는 Screen좌표를 기준으로 하는 String을 Drawing 한다. // The function DrawTextCanvas below draws a String based on the screen coordinates.
+		// 색상 파라미터를 EGUIViewImageLayerTransparencyColor 으로 넣어주게되면 배경색으로 처리함으로 불투명도를 0으로 한것과 같은 효과가 있다. // If the color parameter is added as EGUIViewImageLayerTransparencyColor, it has the same effect as setting the opacity to 0 by processing it as a background color.
+		// 파라미터 순서 : 레이어 -> 기준 좌표 Figure 객체 -> 문자열 -> 폰트 색 -> 면 색 -> 폰트 크기 -> 실제 크기 유무 -> 각도 ->
+		//                 얼라인 -> 폰트 이름 -> 폰트 알파값(불투명도) -> 면 알파값 (불투명도) -> 폰트 두께 -> 폰트 이텔릭
+		// Parameter order: layer -> reference coordinate Figure object -> string -> font color -> Area color -> font size -> actual size -> angle ->
+		//                  Align -> Font Name -> Font Alpha Value (Opaqueness) -> Cotton Alpha Value (Opaqueness) -> Font Thickness -> Font Italic
+		if((res = layerLearn.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Learn Image", YELLOW, BLACK, 20)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to draw text\n");
+			break;
+		}
+
+		if((res = layerSrc.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Source Image", YELLOW, BLACK, 20)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to draw text\n");
+			break;
+		}
+
+		if((res = layerDst.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Destination Image", YELLOW, BLACK, 20)).IsFail())
+		{
+			ErrorPrint(res, L"Failed to draw text\n");
+			break;
+		}
+
+		// 이미지 뷰를 갱신 합니다. // Update image view
+		viewImageLearn.Invalidate(true);
+		viewImageSrc.Invalidate(true);
+		viewImageDst.Invalidate(true);
+
+		// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+		while(viewImageLearn.IsAvailable() && viewImageSrc.IsAvailable() && viewImageDst.IsAvailable())
+			CThreadUtilities::Sleep(1);
+	}
+	while(false);
+
+	return 0;
+}
