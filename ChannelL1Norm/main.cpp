c3e412866e62289a41b3c32c68aa595ac1b9f1bf
jslee <jslee@fourthlogic.co.kr>
2024-10-29 18:00:05 +0900

    Modify Channel Norm Examples

--- ChannelL1Norm/main.cpp
+++ ChannelL1Norm/main.cpp
@@ -20,7 +20,7 @@ int main()
 		CResult res = EResult_UnknownError;
 
 		// 이미지 로드 // Loads image
-		if(IsFail(res = fliSourceImage.Load(L"../../ExampleImages/ChannelL1Norm/Color.flif")))
+		if(IsFail(res = fliSourceImage.Load(L"../../ExampleImages/ChannelL1Norm/Coord.flif")))
 		{
 			ErrorPrint(res, "Failed to load the image file.\n");
 			break;
