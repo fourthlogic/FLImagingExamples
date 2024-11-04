61ab11e1c816f09aefcace43bd08d6f18a2c1af8
sypark <sypark@fourthlogic.co.kr>
2024-11-04 14:52:15 +0900

    [TreeCtrlExample] Avoid flickering

--- TreeCtrlExample/TreeCtrlExampleDlg.cpp
+++ TreeCtrlExample/TreeCtrlExampleDlg.cpp
@@ -179,6 +179,7 @@ BOOL CTreeCtrlExampleDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
+	ModifyStyleEx(0, WS_EX_COMPOSITED);
 	BuildTree();
 
 	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
