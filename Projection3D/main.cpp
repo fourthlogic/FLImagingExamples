70cdcaa425d127229eb4bbc589710fa6551ee3f1
mjjeong <mjjeong@fourthlogic.co.kr>
2024-11-20 11:23:37 +0900

    Fix Projection 3D Example

--- Projection3D/main.cpp
+++ Projection3D/main.cpp
@@ -25,16 +25,16 @@ int main()
 		}
 
 		// Source 3D 뷰 생성
-		if(IsFail(res = view3DSrc.Create(612, 0, 1124, 512)))
+		if((res = view3DSrc.Create(612, 0, 1124, 512)).IsFail())
 		{
-			ErrorPrint(res, "Failed to create the Destination 3D view.\n");
+			ErrorPrint(res, "Failed to create the Source 3D view.\n");
 			break;
 		}
 
 		// Dst 3D 뷰 생성
-		if(IsFail(res = viewImgDst.Create(1124, 0, 1636, 512)))
+		if((res = viewImgDst.Create(1124, 0, 1636, 512)).IsFail())
 		{
-			ErrorPrint(res, "Failed to create the Dst Image view.\n");
+			ErrorPrint(res, "Failed to create the Destination Image view.\n");
 			break;
 		}
 
@@ -50,13 +50,13 @@ int main()
 		
 
 		// Projection3D 객체 생성 // Create Projection3D object
-		CProjection3D Projection3D;
+		CProjection3D projection3D;
 
 		// Destination object 설정 // Set the destination object
-		Projection3D.SetDestinationImage(fliDst);
+		projection3D.SetDestinationImage(fliDst);
 		// Source object 설정 // Set the source object
-		Projection3D.SetSourceObject(floSrc);
-		Projection3D.SetImageSize(512, 512);
+		projection3D.SetSourceObject(floSrc);
+		projection3D.SetImageSize(512, 512);
 
 		//3D View의 카메라 파라미터 값을 초기화하기 위하여 먼저 호출
 		view3DSrc.ZoomFit();
@@ -74,39 +74,34 @@ int main()
 		float f32AovY = cam.GetAngleOfViewY();
 		float f32TargetDistance = cam.GetDistanceFromTarget();
 
-		Projection3D.SetCameraPosition(ptCamPos);
-		Projection3D.SetCameraDirection(ptCamDir);
-		Projection3D.SetDirectionUp(ptCamDirUp);
+		projection3D.SetCameraPosition(ptCamPos);
+		projection3D.SetCameraDirection(ptCamDir);
+		projection3D.SetDirectionUp(ptCamDirUp);
 
-		Projection3D.SetAngleOfView(f32AovX, f32AovY);
-		Projection3D.SetWorkingDistance(f32TargetDistance);
+		projection3D.SetAngleOfView(f32AovX, f32AovY);
+		projection3D.SetWorkingDistance(f32TargetDistance);
 
 		// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
-		if((res = Projection3D.Execute()).IsFail())
+		if((res = projection3D.Execute()).IsFail())
 		{
 			ErrorPrint(res, L"Failed to execute Projection 3D.");
 			break;
 		}
 
-		// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
-		// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately		
+		// 화면에 출력하기 위해 View에서 레이어 0번을 얻어옴 // Obtain layer number 0 from view for display
+		// 이 객체는 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to the view and does not need to be released separately		
 
-		CGUIView3DLayerWrap layer3DSource = view3DSrc.GetLayer(0);
+		CGUIView3DLayerWrap layer3DSrc = view3DSrc.GetLayer(0);
 		CGUIViewImageLayerWrap layer3DDst = viewImgDst.GetLayer(0);
 
 		// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
-		layer3DSource.Clear();
+		layer3DSrc.Clear();
 		layer3DDst.Clear();
 
-		// View 정보를 디스플레이 한다. // Display view information
-		// 아래 함수 DrawTextCanvas 는 Screen좌표를 기준으로 하는 String을 Drawing 한다. // The function DrawTextCanvas below draws a String based on the screen coordinates.
-		// 색상 파라미터를 EGUIViewImageLayerTransparencyColor 으로 넣어주게되면 배경색으로 처리함으로 불투명도를 0으로 한것과 같은 효과가 있다. // If the color parameter is added as EGUIViewImageLayerTransparencyColor, it has the same effect as setting the opacity to 0 by processing it as a background color.
-		// 파라미터 순서 : 레이어 -> 기준 좌표 Figure 객체 -> 문자열 -> 폰트 색 -> 면 색 -> 폰트 크기 -> 실제 크기 유무 -> 각도 ->
-		//                 얼라인 -> 폰트 이름 -> 폰트 알파값(불투명도) -> 면 알파값 (불투명도) -> 폰트 두께 -> 폰트 이텔릭
-		// Parameter order: layer -> reference coordinate Figure object -> string -> font color -> Area color -> font size -> actual size -> angle ->
-		//                  Align -> Font Name -> Font Alpha Value (Opaqueness) -> Cotton Alpha Value (Opaqueness) -> Font Thickness -> Font Italic
+		// View 정보를 디스플레이한다. // Display view information
+		// 함수 DrawTextCanvas는 Screen 좌표를 기준으로 하는 문자열을 드로잉한다. // The function DrawTextCanvas below draws a String based on the screen coordinates.
 		CFLPoint<double> flpTopLeft(0, 0);
-		if((res = layer3DSource.DrawTextCanvas(&flpTopLeft, L"Source Object", YELLOW, BLACK, 20)).IsFail() ||
+		if((res = layer3DSrc.DrawTextCanvas(&flpTopLeft, L"Source Object", YELLOW, BLACK, 20)).IsFail() ||
 		   (res = layer3DDst.DrawTextCanvas(&flpTopLeft, L"Destination Image", YELLOW, BLACK, 20)).IsFail())
 		{
 			ErrorPrint(res, L"Failed to draw text.\n");
@@ -120,8 +115,8 @@ int main()
 			break;
 		}
 
-		//view3DSrc.ZoomFit();
-		viewImgDst.ZoomFit();
+		//Source View와 동일한 시점을 유지하기 위해, 이미지의 정중앙을 뷰의 중앙에 맞추고 배율을 1로 설정
+		viewImgDst.MoveCenterImageCoord(CFLPointD(256, 256), 1.0);
 
 		// 이미지 뷰를 갱신 합니다. // Update image view
 		view3DSrc.Invalidate(true);
