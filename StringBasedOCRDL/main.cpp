19e90f8719a26db7e5a4414c99652af37c2fa77f
sbno <sbno@fourthlogic.co.kr>
2025-01-16 14:37:29 +0900

    Modify StringBasedOCR Example

--- StringBasedOCRDL/main.cpp
+++ StringBasedOCRDL/main.cpp
@@ -180,7 +180,7 @@ int main()
 		// AugmentationSpec ¼³Á¤ // Set the AugmentationSpec
 		CAugmentationSpec augSpec;
 
-		augSpec.SetCommonActivationRatio(0.5);
+		augSpec.SetCommonActivationRatio(0.8);
 		augSpec.SetCommonIoUThreshold(0.8);
 		augSpec.SetCommonInterpolationMethod(FLImaging::ImageProcessing::EInterpolationMethod_Bilinear);
 		augSpec.EnableRotation(true);
