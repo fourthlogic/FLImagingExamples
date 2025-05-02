2966ef7299aee085606d981d84c7b6a929c3c037
shhwang <shhwang@fourthlogic.co.kr>
2025-05-02 21:44:22 +0900

    Add Signed Square Difference Example

---
+++ OperationSignedSquareDifference_Image/main.cpp
@@ -0,0 +1,180 @@
+﻿#include <cstdio>
+#include "../CommomHeader/ErrorPrint.h"
+
+#include <FLImaging.h>
+
+
+int main()
+{
+	// 이미지 객체 선언 // Declare image object
+	CFLImage fliSourceImage;
+	CFLImage fliOperandImage;
+	CFLImage fliDestinationImage;
+
+	// 이미지 뷰 선언 // Declare image view
+	CGUIViewImageWrap viewImageSource;
+	CGUIViewImageWrap viewImageOperand;
+	CGUIViewImageWrap viewImageDestination;
+
+	do
+	{
+        // 알고리즘 동작 결과 // Algorithmic executation result
+		CResult res = EResult_UnknownError;
+
+		// Source 이미지 로드 // Load the source image
+		if(IsFail(res =fliSourceImage.Load(L"../../ExampleImages/OperationSignedSquareDifference/Valley1.flif")))
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		// Operand 이미지 로드 // Loads the operand image
+		if(IsFail(res =fliOperandImage.Load(L"../../ExampleImages/OperationSignedSquareDifference/Valley2.flif")))
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		// Source 이미지 뷰 생성 // Create Source image view
+		if(IsFail(res =viewImageSource.Create(100, 0, 612, 512)))
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// Operand 이미지 뷰 생성 // Creates operand image view
+		if(IsFail(res =viewImageOperand.Create(612, 0, 1124, 512)))
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// Destination 이미지 뷰 생성 // Create destination image view
+		if(IsFail(res =viewImageDestination.Create(1124, 0,1636, 512)))
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// Source 이미지 뷰와 Operand 이미지 뷰의 시점을 동기화 한다
+		if(IsFail(res =viewImageSource.SynchronizePointOfView(&viewImageOperand)))
+		{
+			ErrorPrint(res, "Failed to synchronize view\n");
+			break;
+		}
+
+		// Source 이미지 뷰와 Destination 이미지 뷰의 시점을 동기화 한다
+		if(IsFail(res =viewImageSource.SynchronizePointOfView(&viewImageDestination)))
+		{
+			ErrorPrint(res, "Failed to synchronize view\n");
+			break;
+		}
+
+		// Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
+		if(IsFail(res =viewImageSource.SetImagePtr(&fliSourceImage)))
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// Operand 이미지 뷰에 이미지를 디스플레이
+		if(IsFail(res =viewImageOperand.SetImagePtr(&fliOperandImage)))
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// Destination 이미지 뷰에 이미지를 디스플레이 // Display the image in the destination image view
+		if(IsFail(res =viewImageDestination.SetImagePtr(&fliDestinationImage)))
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// 두 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the two image view windows
+		if(IsFail(res =viewImageSource.SynchronizeWindow(&viewImageOperand)))
+		{
+			ErrorPrint(res, "Failed to synchronize window.\n");
+			break;
+		}
+
+		// 두 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the two image view windows
+		if(IsFail(res =viewImageSource.SynchronizeWindow(&viewImageDestination)))
+		{
+			ErrorPrint(res, "Failed to synchronize window.\n");
+			break;
+		}
+
+		// Operation SignedSquareDifference 객체 생성 // Create Operation SignedSquareDifference object
+		COperationSignedSquareDifference signedSquareDifference;
+
+		// Source 이미지 설정 // Set the source image
+		signedSquareDifference.SetSourceImage(fliSourceImage);
+
+		// Operand 이미지 설정 // Set the operand image
+		signedSquareDifference.SetOperandImage(fliOperandImage);
+
+		// Destination 이미지 설정 // Set the destination image
+		signedSquareDifference.SetDestinationImage(fliDestinationImage);
+
+		// Operation Overflow Method 설정 // Set operation overflow method
+		signedSquareDifference.SetOverflowMethod(EOverflowMethod_Clamping);
+
+		// Image Operation 모드로 설정 // Set operation mode to image
+		signedSquareDifference.SetOperationSource(EOperationSource_Image);
+
+		// 공백 색상 칠하기 모드 해제
+		// 결과 이미지가 이미 존재할 경우 연산되지 않은 영역을 공백 색상으로 칠하지 않고 원본 그대로 둔다.
+		signedSquareDifference.EnableFillBlankColorMode(false);
+
+		// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+		if(IsFail(res =res = signedSquareDifference.Execute()))
+		{
+			ErrorPrint(res, "Failed to execute operation signedSquareDifference.\n");
+			break;
+		}
+
+		// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+		// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+		CGUIViewImageLayerWrap layerSource = viewImageSource.GetLayer(0);
+		CGUIViewImageLayerWrap layerOperand = viewImageOperand.GetLayer(0);
+		CGUIViewImageLayerWrap layerDestination = viewImageDestination.GetLayer(0);
+
+		// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+		layerSource.Clear();
+		layerOperand.Clear();
+		layerDestination.Clear();
+
+
+		// 이미지 뷰 정보 표시 // Display image view information
+		if(IsFail(res =layerSource.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Source Image", YELLOW, BLACK, 30)))
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		if(IsFail(res =layerOperand.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Operand Image", YELLOW, BLACK, 30)))
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		if(IsFail(res =layerDestination.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Destination Image", YELLOW, BLACK, 30)))
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		// 이미지 뷰를 갱신 // Update image view
+		viewImageSource.Invalidate(true);
+		viewImageOperand.Invalidate(true);
+		viewImageDestination.Invalidate(true);
+
+		// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+		while(viewImageSource.IsAvailable() && viewImageOperand.IsAvailable() && viewImageDestination.IsAvailable())
+			CThreadUtilities::Sleep(1);
+	}
+	while(false);
+
+	return 0;
+}
---
+++ OperationSignedSquareDifference_Scalar/main.cpp
@@ -0,0 +1,132 @@
+﻿#include <cstdio>
+#include "../CommomHeader/ErrorPrint.h"
+
+#include <FLImaging.h>
+
+
+int main()
+{
+	// 이미지 객체 선언 // Declare image object
+	CFLImage fliSourceImage;
+	CFLImage fliDestinationImage;
+
+	// 이미지 뷰 선언 // Declare image view
+	CGUIViewImageWrap viewImageSource;
+	CGUIViewImageWrap viewImageDestination;
+
+	do
+	{
+		// 알고리즘 동작 결과 // Algorithmic executation result
+		CResult res = EResult_UnknownError;
+
+		// Source 이미지 로드 // Load the source image
+		if(IsFail(res = fliSourceImage.Load(L"../../ExampleImages/OperationSignedSquareDifference/Gradation.flif")))
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		// Source 이미지 뷰 생성 // Create Source image view
+		if(IsFail(res = viewImageSource.Create(100, 0, 612, 512)))
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// Destination 이미지 뷰 생성 // Create destination image view
+		if(IsFail(res = viewImageDestination.Create(612, 0, 1124, 512)))
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views
+		if(IsFail(res = viewImageSource.SynchronizePointOfView(&viewImageDestination)))
+		{
+			ErrorPrint(res, "Failed to synchronize view\n");
+			break;
+		}
+
+		// Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
+		if(IsFail(res = viewImageSource.SetImagePtr(&fliSourceImage)))
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// Destination 이미지 뷰에 이미지를 디스플레이 // Display the image in the destination image view
+		if(IsFail(res = viewImageDestination.SetImagePtr(&fliDestinationImage)))
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// 두 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the two image view windows
+		if(IsFail(res = viewImageSource.SynchronizeWindow(&viewImageDestination)))
+		{
+			ErrorPrint(res, "Failed to synchronize window.\n");
+			break;
+		}
+
+		// Operation SignedSquareDifference 객체 생성 // Create Operation SignedSquareDifference object
+		COperationSignedSquareDifference signedSquareDifference;
+
+		// Source 이미지 설정 // Set the source image
+		signedSquareDifference.SetSourceImage(fliSourceImage);
+
+		// Destination 이미지 설정 // Set the destination image
+		signedSquareDifference.SetDestinationImage(fliDestinationImage);
+
+		// Operation Overflow Method 설정 // Set operation overflow method
+		signedSquareDifference.SetOverflowMethod(EOverflowMethod_Wrapping);
+
+		// Scalar Operation 모드로 설정 // Set operation mode to scalar
+		signedSquareDifference.SetOperationSource(EOperationSource_Scalar);
+
+		// 스칼라 색상 지정
+		signedSquareDifference.SetScalarValue(CMultiVar<double>(0));
+
+		// 공백 영역을 지정한 색으로 채우지 않도록 설정
+		signedSquareDifference.EnableFillBlankColorMode(false);
+
+		// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+		if(IsFail(res = res = signedSquareDifference.Execute()))
+		{
+			ErrorPrint(res, "Failed to execute operation signedSquareDifference.\n");
+			break;
+		}
+
+		// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+		// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+		CGUIViewImageLayerWrap layerSource = viewImageSource.GetLayer(0);
+		CGUIViewImageLayerWrap layerDestination = viewImageDestination.GetLayer(0);
+
+		// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+		layerSource.Clear();
+		layerDestination.Clear();
+
+		// 이미지 뷰 정보 표시 // Display image view information
+		if(IsFail(res = layerSource.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Source Image", YELLOW, BLACK, 30)))
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		if(IsFail(res = layerDestination.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Destination Image", YELLOW, BLACK, 30)))
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		// 이미지 뷰를 갱신 // Update image view
+		viewImageSource.Invalidate(true);
+		viewImageDestination.Invalidate(true);
+
+		// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+		while(viewImageSource.IsAvailable() && viewImageDestination.IsAvailable())
+			CThreadUtilities::Sleep(1);
+	}
+	while(false);
+
+	return 0;
+}
