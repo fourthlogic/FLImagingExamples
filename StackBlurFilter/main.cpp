8c35f0ac11c738af4af9df4c3ddcfd2310c02e22
jjpark <jjpark@fourthlogic.co.kr>
2025-05-20 11:41:52 +0900

    Modify Stack Blur Filter Example

--- StackBlurFilter/main.cpp
+++ StackBlurFilter/main.cpp
@@ -97,8 +97,8 @@ int main()
 		// Destination 이미지 설정 // Set the destination image
 		stackBlurFilter.SetDestinationImage(fliDstImage);
 
-		// 처리할 Filter의 Radius 설정 // Set the radius of the filter
-		stackBlurFilter.SetRadius(5);
+		// 처리할 Filter의 커널 크기 설정 // Set the kernel of the filter
+		stackBlurFilter.SetKernel(11);
 
 		// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
 		if(IsFail(res = stackBlurFilter.Execute()))
