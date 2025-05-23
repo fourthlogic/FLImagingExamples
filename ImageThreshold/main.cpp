b9d6398b2dfb19244ef9a622d5d15a489613293f
jjpark <jjpark@fourthlogic.co.kr>
2025-05-23 09:00:26 +0900

    Add Image Threshold Example

---
+++ ImageThreshold/main.cpp
@@ -0,0 +1,168 @@
+﻿#include <cstdio>
+
+#include <FLImaging.h>
+#include "../CommomHeader/ErrorPrint.h" 
+
+
+int main()
+{
+	// 이미지 객체 선언 // Declare the image object
+	CFLImage fliISrcImage;
+	CFLImage fliIOprImage;
+	CFLImage fliIDstImage;
+
+	// 이미지 뷰 선언 // Declare the image view
+	CGUIViewImageWrap viewImage[3];
+
+	do
+	{
+		// 동작 결과 // operation result
+		CResult res = EResult_UnknownError;
+
+		// 이미지 로드 // Load image
+		if(IsFail(res = fliISrcImage.Load(L"../../ExampleImages/ImageThreshold/SunSrc.flif")))
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		if(IsFail(res = fliIOprImage.Load(L"../../ExampleImages/ImageThreshold/SunOpr.flif")))
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		// 이미지 뷰 생성 // Create image view
+		if(IsFail(res = viewImage[0].Create(300, 0, 300 + 384, 384)))
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		if(IsFail(res = viewImage[1].Create(300 + 384, 0, 300 + 384 * 2, 384)))
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		if(IsFail(res = viewImage[2].Create(300 + 384 * 2, 0, 300 + 384 * 3, 384)))
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
+		// 이미지 뷰에 이미지를 디스플레이 // Display the image in the image view
+		if(IsFail(res = viewImage[0].SetImagePtr(&fliISrcImage)))
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		if(IsFail(res = viewImage[1].SetImagePtr(&fliIOprImage)))
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		if(IsFail(res = viewImage[2].SetImagePtr(&fliIDstImage)))
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the image views. 
+		if(IsFail(res = viewImage[0].SynchronizePointOfView(&viewImage[1])))
+		{
+			ErrorPrint(res, "Failed to synchronize view\n");
+			break;
+		}
+
+		if(IsFail(res = viewImage[0].SynchronizePointOfView(&viewImage[2])))
+		{
+			ErrorPrint(res, "Failed to synchronize view\n");
+			break;
+		}
+
+		// 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the image view windows
+		if(IsFail(res = viewImage[0].SynchronizeWindow(&viewImage[1])))
+		{
+			ErrorPrint(res, "Failed to synchronize window\n");
+			break;
+		}
+
+		if(IsFail(res = viewImage[0].SynchronizeWindow(&viewImage[2])))
+		{
+			ErrorPrint(res, "Failed to synchronize window\n");
+			break;
+		}
+
+		// Image Threshold 객체 생성 // Create Image Threshold object
+		CImageThreshold threshold;
+
+		// Source 이미지 설정 // Set source image 
+		threshold.SetSourceImage(fliISrcImage);
+
+		// Operand 이미지 설정 // Set operand image 
+		threshold.SetOperandImage(fliIOprImage);
+
+		// Destination 이미지 설정 // Set destination image
+		threshold.SetDestinationImage(fliIDstImage);
+
+		// 임계값 오프셋 설정 // set threshold offset 
+		threshold.SetThresholdOffset(5);
+
+		// 알고리즘 수행 // Execute the algorithm
+		if((res = threshold.Execute()).IsFail())
+		{
+			ErrorPrint(res, "Failed to execute Image Threshold.");
+			break;
+		}
+
+		// 출력을 위한 이미지 레이어를 얻어옵니다. //  Gets the image layer for output.
+		// 따로 해제할 필요 없음 // No need to release separately
+		CGUIViewImageLayerWrap layerSource = viewImage[0].GetLayer(0);
+		CGUIViewImageLayerWrap layerOperand = viewImage[1].GetLayer(0);
+		CGUIViewImageLayerWrap layerDestination = viewImage[2].GetLayer(0);
+
+		// 기존에 Layer에 그려진 도형들을 삭제 // Delete the shapes drawn on the existing layer
+		layerSource.Clear();
+		layerOperand.Clear();
+		layerDestination.Clear();
+
+		// View 정보를 디스플레이 합니다. // Display View information.
+		// 아래 함수 DrawTextCanvas 는 Screen좌표를 기준으로 하는 String을 Drawing 한다.// The function DrawTextCanvas below draws a String based on the screen coordinates.
+		// 파라미터 순서 : 레이어 -> 기준 좌표 Figure 객체 -> 문자열 -> 폰트 색 -> 면 색 -> 폰트 크기 -> 실제 크기 유무 -> 각도 ->
+		//                 얼라인 -> 폰트 이름 -> 폰트 알파값(불투명도) -> 면 알파값 (불투명도) -> 폰트 두께 -> 폰트 이텔릭
+		// Parameter order: layer -> reference coordinate Figure object -> string -> font color -> Area color -> font size -> actual size -> angle ->
+		//                  Align -> Font Name -> Font Alpha Value (Opaqueness) -> Cotton Alpha Value (Opaqueness) -> Font Thickness -> Font Italic
+		if(IsFail(res = layerSource.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Source Image", YELLOW, BLACK, 30)))
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		if(IsFail(res = layerOperand.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Operand Image", YELLOW, BLACK, 30)))
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		if(IsFail(res = layerDestination.DrawTextCanvas(&CFLPoint<double>(0, 0), L"Destination Image", YELLOW, BLACK, 30)))
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
+		// 이미지 뷰를 갱신 합니다. // Update the image view.
+		viewImage[0].Invalidate(true);
+		viewImage[1].Invalidate(true);
+		viewImage[2].Invalidate(true);
+
+		// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+		while(viewImage[0].IsAvailable() && viewImage[1].IsAvailable() && viewImage[2].IsAvailable())
+			CThreadUtilities::Sleep(1);
+	}
+	while(false);
+
+	return 0;
+}
