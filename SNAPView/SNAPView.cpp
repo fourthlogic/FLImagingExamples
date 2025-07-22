246b0162ddb6a5ef3002e863131018968eab8c12
ks <ks@fourthlogic.co.kr>
2025-07-22 18:01:37 +0900

    Rename Example File Names

--- SNAPView/SNAPView.cpp
+++ SNAPView/SNAPView.cpp
@@ -10,21 +10,28 @@ int main()
 
 	do 
 	{
-		// 스냅 빌드 객체 선언 // Declare SNAP Build
-		CSNAPBuild snapBuild;
+		// 스냅 뷰 객체 선언 // Declare SNAP View
+		CGUIViewSNAPWrap viewSNAP;
+
+		// 스냅 뷰 생성 // Create SNAP view
+		if((res = viewSNAP.Create(0, 0, 600, 600)).IsFail())
+		{
+			ErrorPrint(res, "Failed to create the SNAP view.\n");
+			break;
+		}
 
 		// 스냅 파일 로드 // Load SNAP file
-		if((res = snapBuild.Load(L"C:\\Users\\Public\\Documents\\FLImaging\\ExamplesSNAP\\Advanced Functions\\Object\\Blob.flsf")).IsFail())
+		if((res = viewSNAP.Load(L"C:\\Users\\Public\\Documents\\FLImaging\\FLImagingExamplesSNAP\\Advanced Functions\\Object\\Blob.flsf")).IsFail())
 		{
 			ErrorPrint(res, "Failed to load the SNAP file.\n");
 			break;
 		}
 
 		// 스냅 실행 // Run SNAP
-		res = snapBuild.Run();
+		res = viewSNAP.Run();
 
 		// 스냅이 종료될 때 까지 기다림 // Wait for the SNAP to close
-		while(snapBuild.IsAvailable())
+		while(viewSNAP.IsAvailable())
 			CThreadUtilities::Sleep(1);
 	}
 	while(false);
